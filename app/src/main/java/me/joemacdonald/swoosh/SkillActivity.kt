38197981c509dd72a31435c38d40bf6d506bd7d4
JoeM <joem2525@gmail.com>
2017-12-04 14:53:55 -0500

    lesson 04 - Third Activity

---
+++ app/src/main/java/me/joemacdonald/swoosh/SkillActivity.kt
@@ -0,0 +1,14 @@
+package me.joemacdonald.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+
+
+}
